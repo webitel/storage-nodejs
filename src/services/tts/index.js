3ab780cc5c9a3def3cf23b6b9cdd50e5ffebc3c8
Navrotskyj <navrotskyj@gmail.com>
2017-10-10 19:05:25 +0300

    WTEL-534

--- src/services/tts/index.js
+++ src/services/tts/index.js
@@ -249,16 +249,39 @@ function microsoftLocalesNameMaping(locale, gender) {
         case 'ar-EG':
             return "ar-EG, Hoda";
 
-        case 'id-ID':
-            return 'id-ID, Andika';
+        case 'ar-SA':
+            return "ar-SA, Naayf";
+
+        case 'ca-ES':
+            return "ca-ES, HerenaRUS";
+
+        case 'cs-CZ':
+            return "cs-CZ, Vit";
+
+        case 'da-DK':
+            return "da-DK, HelleRUS";
+
+        case 'de-AT':
+            return "de-AT, Michael";
+
+        case 'de-CH':
+            return "de-CH, Karsten";
 
         case 'de-DE':
             if (isFemale(gender))
                 return "de-DE, Hedda";
             else return "de-DE, Stefan, Apollo";
 
+        case 'el-GR':
+            return "el-GR, Stefanos";
+
         case 'en-AU':
             return "en-AU, Catherine";
+
+        case 'id-ID':
+            return 'id-ID, Andika';
+
+
         case 'en-CA':
             return "en-CA, Linda";
 
@@ -267,8 +290,13 @@ function microsoftLocalesNameMaping(locale, gender) {
                 return "en-GB, Susan, Apollo";
             else return "en-GB, George, Apollo";
 
+        case 'en-IE':
+            return "en-IE, Shaun";
+
         case 'en-IN':
-            return "en-IN, Ravi, Apollo";
+            if (isFemale(gender))
+                return "en-IN, Heera, Apollo";
+            else return "en-IN, Ravi, Apollo";
 
         case 'en-US':
             if (isFemale(gender))
@@ -281,15 +309,33 @@ function microsoftLocalesNameMaping(locale, gender) {
             else return "es-ES, Pablo, Apollo";
 
         case 'es-MX':
-            return "es-MX, Raul, Apollo";
+            if (isFemale(gender))
+                return "es-MX, HildaRUS";
+            else return "es-MX, Raul, Apollo";
+
+        case 'fi-FI':
+            return "fi-FI, HeidiRUS";
 
         case 'fr-CA':
-            return "fr-CA, Caroline";
+            if (isFemale(gender))
+                return "fr-CA, Caroline";
+            else return "fr-CH, Guillaume";
 
         case 'fr-FR':
             if (isFemale(gender))
                 return "fr-FR, Julie, Apollo";
             else return "fr-FR, Paul, Apollo";
+
+        case 'he-IL':
+            return "he-IL, Asaf";
+
+        case 'hi-IN':
+            if (isFemale(gender))
+                return "hi-IN, Kalpana, Apollo";
+            else return "hi-IN, Hemant";
+
+        case 'hu-HU':
+            return "hu-HU, Szabolcs";
 
         case 'it-IT':
             return "it-IT, Cosimo, Apollo";
@@ -299,13 +345,45 @@ function microsoftLocalesNameMaping(locale, gender) {
                 return "ja-JP, Ayumi, Apollo";
             else return "ja-JP, Ichiro, Apollo";
 
+        case 'ko-KR':
+            return "ko-KR, HeamiRUS";
+
+        case 'nb-NO':
+            return "nb-NO, HuldaRUS";
+
+        case 'nl-NL':
+            return "nl-NL, HannaRUS";
+
+        case 'pl-PL':
+            return "pl-PL, PaulinaRUS";
+
         case 'pt-BR':
-            return "pt-BR, Daniel, Apollo";
+            if (isFemale(gender))
+                return "pt-BR, HeloisaRUS";
+            else return "pt-BR, Daniel, Apollo";
+
+        case 'pt-PT':
+            return "pt-PT, HeliaRUS";
+
+        case 'ro-RO':
+            return "ro-RO, Andrei";
 
         case 'ru-RU':
             if (isFemale(gender))
-                return "pt-BR, Daniel, Apollo";
+                return "ru-RU, Irina, Apollo";
             else return "ru-RU, Pavel, Apollo";
+
+        case 'sk-SK':
+            return "sk-SK, Filip";
+
+        case 'sv-SE':
+            return "sv-SE, HedvigRUS";
+
+        case 'th-TH':
+            return "th-TH, Pattara";
+
+        case 'tr-TR':
+            return "tr-TR, SedaRUS";
 
         case 'zh-CN':
             if (isFemale(gender))
@@ -320,7 +398,7 @@ function microsoftLocalesNameMaping(locale, gender) {
         case 'zh-TW':
             if (isFemale(gender))
                 return "zh-TW, Yating, Apollo";
-            else return "zh-TW, Yating, Apollo";
+            else return "zh-TW, Zhiwei, Apollo";
 
         default:
             log.error(`unknown local: ${locale}`);
