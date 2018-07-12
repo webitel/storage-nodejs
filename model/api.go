29b0d6ab694c96b2e3fde5a1d405bf113c4989c8
Navrotskyj <navrotskyj@gmail.com>
2018-07-12 12:12:10 +0300

    bug

--- model/api.go
+++ model/api.go
@@ -1,9 +1,9 @@
 package model
 
 const (
-	API_URL_SUFFIX_V4          = "/api/v2"
+	API_URL_SUFFIX_V2          = "/api/v2"
 	API_INTERNAL_URL_SUFFIX_V1 = "/sys"
-	API_URL_SUFFIX             = API_URL_SUFFIX_V4
+	API_URL_SUFFIX             = API_URL_SUFFIX_V2
 
 	HEADER_REQUEST_ID         = "X-Request-ID"
 	HEADER_TOKEN              = "X-Access-Token"
