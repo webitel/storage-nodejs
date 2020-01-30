3a4d038748adfd0d8ea1bdeab89d5e601519d62d
navrotskyj <navrotskyj@gmail.com>
2020-01-30 12:40:16 +0200

    added protobuf go_package

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -1,10 +1,10 @@
-protoc -I/usr/local/include -I../grpc_api/proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  \
-  -I/home/igor/opt/Programs/golib/src/github.com/grpc-ecosystem/grpc-gateway \
+protoc -I/usr/local/include -I../grpc_api/protos -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
   -I$GOPATH/src/github.com/webitel/engine/grpc_api/protos \
   --swagger_out=version=false,json_names_for_fields=false,allow_delete_body=true,include_package_in_tags=false,allow_repeated_fields_in_body=false,fqn_for_swagger_name=false,merge_file_name=api,allow_merge=true:./api \
-  ../grpc_api/proto/*.proto
+  ../grpc_api/protos/*.proto
 
-protoc -I/usr/local/include -I../grpc_api/proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  \
+protoc -I/usr/local/include -I../grpc_api/protos -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  \
   -I/home/igor/opt/Programs/golib/src/github.com/grpc-ecosystem/grpc-gateway \
   -I$GOPATH/src/github.com/webitel/engine/grpc_api/protos \
-   --go_out=plugins=grpc:../grpc_api/storage ../grpc_api/proto/*.proto
+   --go_out=plugins=grpc,paths=source_relative:../grpc_api/storage ../grpc_api/protos/*.proto
